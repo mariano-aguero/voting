17467e762ac501e7a9fc8dcafcb411a839327e2f
Mariano Aguero <mariano.aguero@gmail.com>
2018-05-09 01:10:43 -0300

    [feat](Vote DAPP): Better indent style
    [QA]:
    Has unit tests:
    Reviewed by:
    
    [TRELLO]:
    https://trello.com/b/9hn1TUuZ/voting-dapp

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react';
-import {Container, Jumbotron, Button, ListGroup, ListGroupItem, ButtonToolbar} from 'reactstrap';
+import {Component} from 'react';
+import {Button, ButtonToolbar, Container, Jumbotron, ListGroup, ListGroupItem} from 'reactstrap';
 import VoteContract from '../build/contracts/Vote.json';
 import getWeb3 from './utils/getWeb3';
 import Alert from 'react-s-alert';
 
 import './App.css';
-
 // Bootstrap
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
 // Alert message
 import './../node_modules/react-s-alert/dist/s-alert-default.css';
 
@@ -17,198 +15,198 @@ const voteNo = 'no';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      hidden: "hidden",
-      web3: null,             // variable that manage web3 instance
-      contractInstance: null, // variable that manage the vote contract instance
-      buttonSubmitted: null,  // disable button on submit
-      alreadyVote: false,     // variable that know if an account already vote
-      myVote: null,           // my vote on the contract
-      account: null,          // actual account
-      quantityOfVoteForNo: 0, // results for 'no'
-      quantityOfVoteForYes: 0,// results fot 'yes'
-      latestFilter: null,     // filter to latest block
-      txHash: null            // transaction hash related to the user vote
-    };
+        this.state = {
+            hidden: "hidden",
+            web3: null,             // variable that manage web3 instance
+            contractInstance: null, // variable that manage the vote contract instance
+            buttonSubmitted: null,  // disable button on submit
+            alreadyVote: false,     // variable that know if an account already vote
+            myVote: null,           // my vote on the contract
+            account: null,          // actual account
+            quantityOfVoteForNo: 0, // results for 'no'
+            quantityOfVoteForYes: 0,// results fot 'yes'
+            latestFilter: null,     // filter to latest block
+            txHash: null            // transaction hash related to the user vote
+        };
 
-  }
-
-  /**
-   * Will mount web3 and instantiate
-   * @returns {Promise.<void>}
-   */
-  async componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
-    try {
-      let results = await getWeb3;
-      if (!results || !results.web3) {
-        throw new Error('Error finding web3.');
-      }
-
-      // Setup a filter for the latest block
-      const latestFilter = results.web3.eth.filter('latest');
-
-      this.setState({
-        web3: results.web3,
-        latestFilter: latestFilter
-      });
-
-      // Instantiate contract once web3 provided.
-      this.instantiateContract();
-
-    } catch (err) {
-      let {message = 'Error finding web3.'} = err;
-
-      console.log(message);
-
-      Alert.error(message);
     }
-  }
 
-  /**
-   * This function instantiate the smart contract used for voting
-   * @returns {Promise.<void>}
-   */
-  async instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
+    /**
+     * Will mount web3 and instantiate
+     * @returns {Promise.<void>}
      */
+    async componentWillMount() {
+        // Get network provider and web3 instance.
+        // See utils/getWeb3 for more info.
+        try {
+            let results = await getWeb3;
+            if (!results || !results.web3) {
+                throw new Error('Error finding web3.');
+            }
 
-    const contract = require('truffle-contract');
-    const voteContract = contract(VoteContract);
-    voteContract.setProvider(this.state.web3.currentProvider);
+            // Setup a filter for the latest block
+            const latestFilter = results.web3.eth.filter('latest');
 
-    this.state.web3.eth.getAccounts(async (error, accounts) => {
-      let instance = await voteContract.deployed();
-      let account = accounts[0];
+            this.setState({
+                web3: results.web3,
+                latestFilter: latestFilter
+            });
 
-      this.setState({
-        alreadyVote: await instance.alreadyVote({from: account})
-      });
+            // Instantiate contract once web3 provided.
+            this.instantiateContract();
 
-      this.setState({
-        myVote: await instance.myVote({from: account}) ? voteYes : voteNo
-      });
+        } catch (err) {
+            let {message = 'Error finding web3.'} = err;
 
-      this.setState({
-        contractInstance: instance
-      });
+            console.log(message);
 
-      this.setState({
-        account: account
-      });
+            Alert.error(message);
+        }
+    }
 
-      let votesForNo = await instance.getVotedNo({from: account});
-      let votesForYes = await instance.getVotedYes({from: account});
+    /**
+     * This function instantiate the smart contract used for voting
+     * @returns {Promise.<void>}
+     */
+    async instantiateContract() {
+        /*
+         * SMART CONTRACT EXAMPLE
+         *
+         * Normally these functions would be called in the context of a
+         * state management library, but for convenience I've placed them here.
+         */
 
-      this.setState({
-        quantityOfVoteForNo: votesForNo.toNumber(),
-        quantityOfVoteForYes: votesForYes.toNumber()
-      });
+        const contract = require('truffle-contract');
+        const voteContract = contract(VoteContract);
+        voteContract.setProvider(this.state.web3.currentProvider);
 
-      // In react when you hide or show a component we need to wait until state of variables are triggered
-      setTimeout(() => {
-        this.setState({
-          hidden: ""
-        });
-      }, 100);
-    })
+        this.state.web3.eth.getAccounts(async (error, accounts) => {
+            let instance = await voteContract.deployed();
+            let account = accounts[0];
 
-  }
+            this.setState({
+                alreadyVote: await instance.alreadyVote({from: account})
+            });
 
-  /**
-   * Handle submit form and interact with smart contract
-   * @param event
-   */
-  async handleClick(vote) {
-    event.preventDefault();
-    try {
+            this.setState({
+                myVote: await instance.myVote({from: account}) ? voteYes : voteNo
+            });
 
-      this.setState({
-        buttonSubmitted: true
-      });
+            this.setState({
+                contractInstance: instance
+            });
 
-      // Handle yes
-      let result;
-      if (vote === voteYes) {
-        result = await this.state.contractInstance.voteYes({
-          from: this.state.account,
-          value: this.state.web3.toWei(0.01, "ether")
-        });
-      }
+            this.setState({
+                account: account
+            });
 
-      // Handle no
-      if (vote === voteNo) {
-        result = await this.state.contractInstance.voteNo({
-          from: this.state.account,
-          value: this.state.web3.toWei(0.01, "ether")
-        });
-      }
+            let votesForNo = await instance.getVotedNo({from: account});
+            let votesForYes = await instance.getVotedYes({from: account});
 
-      // Set hash transaction
-      if(result && result.tx) {
-        this.setState({
-          txHash: result.tx
-        });
-      }
-
-      const {latestFilter, web3} = this.state;
-
-      latestFilter.watch((error, result) => {
-        if (error) {
-          console.error(error);
-        } else {
-          let thisTx = this.state.txHash;
-          console.log(`State transaction for vote ${thisTx}`);
-
-          web3.eth.getBlock('latest', async (e, res) => {
-            let {transactions} = res;
-
-            transactions.map(async (transaction) => {
-              if (thisTx !== transaction) {
-                return transaction;
-              }
-
-              console.log("Got match!");
-
-              let myVote = await this.state.contractInstance.myVote({from: this.state.account}) ? voteYes : voteNo;
-              let votesForNo = await this.state.contractInstance.getVotedNo({from: this.state.account});
-              let votesForYes = await this.state.contractInstance.getVotedYes({from: this.state.account});
-
-              this.setState({
-                myVote: myVote,
+            this.setState({
                 quantityOfVoteForNo: votesForNo.toNumber(),
                 quantityOfVoteForYes: votesForYes.toNumber()
-              });
+            });
 
-              return transaction;
-            })
-          });
-        }
+            // In react when you hide or show a component we need to wait until state of variables are triggered
+            setTimeout(() => {
+                this.setState({
+                    hidden: ""
+                });
+            }, 100);
+        })
 
-        this.setState({
-          alreadyVote: true,
-          buttonSubmitted: false
-        });
-
-        Alert.success('Vote succesfully submitted');
-      })
-
-    } catch (err) {
-      console.error(err);
-      this.setState({
-        buttonSubmitted: false
-      });
-      Alert.error(err.message);
     }
-  }
+
+    /**
+     * Handle submit form and interact with smart contract
+     * @param event
+     */
+    async handleClick(vote) {
+        event.preventDefault();
+        try {
+
+            this.setState({
+                buttonSubmitted: true
+            });
+
+            // Handle yes
+            let result;
+            if (vote === voteYes) {
+                result = await this.state.contractInstance.voteYes({
+                    from: this.state.account,
+                    value: this.state.web3.toWei(0.01, "ether")
+                });
+            }
+
+            // Handle no
+            if (vote === voteNo) {
+                result = await this.state.contractInstance.voteNo({
+                    from: this.state.account,
+                    value: this.state.web3.toWei(0.01, "ether")
+                });
+            }
+
+            // Set hash transaction
+            if (result && result.tx) {
+                this.setState({
+                    txHash: result.tx
+                });
+            }
+
+            const {latestFilter, web3} = this.state;
+
+            latestFilter.watch((error, result) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    let thisTx = this.state.txHash;
+                    console.log(`State transaction for vote ${thisTx}`);
+
+                    web3.eth.getBlock('latest', async (e, res) => {
+                        let {transactions} = res;
+
+                        transactions.map(async (transaction) => {
+                            if (thisTx !== transaction) {
+                                return transaction;
+                            }
+
+                            console.log("Got match!");
+
+                            let myVote = await this.state.contractInstance.myVote({from: this.state.account}) ? voteYes : voteNo;
+                            let votesForNo = await this.state.contractInstance.getVotedNo({from: this.state.account});
+                            let votesForYes = await this.state.contractInstance.getVotedYes({from: this.state.account});
+
+                            this.setState({
+                                myVote: myVote,
+                                quantityOfVoteForNo: votesForNo.toNumber(),
+                                quantityOfVoteForYes: votesForYes.toNumber()
+                            });
+
+                            return transaction;
+                        })
+                    });
+                }
+
+                this.setState({
+                    alreadyVote: true,
+                    buttonSubmitted: false
+                });
+
+                Alert.success('Vote succesfully submitted');
+            })
+
+        } catch (err) {
+            console.error(err);
+            this.setState({
+                buttonSubmitted: false
+            });
+            Alert.error(err.message);
+        }
+    }
 
   /**
    * Function to render the results
